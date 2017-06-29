a35af26d8fa6032f82fb5bcc8c075997192804c1
EC2 Default User <ec2-user@ip-172-31-31-148.ca-central-1.compute.internal>
2017-06-29 21:20:26 +0000

    Implement a google-map example

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT_NUM = 8080;
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/templates/mymap.html'));
+});
+
+app.listen(PORT_NUM, function () {
+  console.log('Example app listening on port '+ PORT_NUM + ' !');
+});
