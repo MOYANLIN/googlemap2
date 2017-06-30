6386518abb5c0343d1782b00c2e5e5d7267ec153
MichaeliaNa <nina.naliu1110@gmail.com>
2017-06-30 03:13:39 +0000

    update git config

--- build_css.sh
+++ build_css.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pushd css && for file in *.less; do lessc $file 'basename $file | sed -e "s/less/css/"' ; done && popd
+pushd css && for file in *.less; do lessc $file `basename $file | sed -e "s/less/css/"` ; done && popd
