1f87bc58457b96aa068446b1098c5660d74b2ac4
MichaeliaNa <nina.naliu1110@gmail.com>
2017-07-06 18:22:49 +0000

    show maps using placeService

--- js/mymap.js
+++ js/mymap.js
@@ -1,36 +1,59 @@
-var DEFAULT_ZOOM = 15;
-var GOOGLE_API_KEY = 'AIzaSyABCvs2XAvZNrltqvmr_Hea-uF6o1sF2-8';
 
 $(function(){
+	var DEFAULT_ZOOM = 15;
+	var GOOGLE_API_KEY = 'AIzaSyABCvs2XAvZNrltqvmr_Hea-uF6o1sF2-8';
+	var DEFAULT_RADIUS = 1500;
+	var DEFAULT_LAT = 43.642509;
+	var DEFAULT_LNG = -79.387039;
+
 	function initMap(){
-		var position = {
-			lat: 37.773972,
-			lng: -122.43129
-		};
 
 		var map = new google.maps.Map($('#map')[0],{
 			zoom: DEFAULT_ZOOM,
-			center: position
+			center: {
+				lat : DEFAULT_LAT,
+				lng : DEFAULT_LNG
+			}
 		});
 
-		var marker = new google.maps.Marker({
-			position: position,
-			map: map
-		});
+		var params = {
+			'location' : new google.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+			'radius' : DEFAULT_RADIUS,
+			'type' : 'restaurant'
+		};
 
-		$.ajax({
-			url: '/nearby_search',
-			data: {
-				'location': position.lat +','+position.lng,
-				'type': 'restaurant',
-				'key':GOOGLE_API_KEY,
-				'radius': 500
-			},
-			success: function(data){
-				debugger;
-			},
-			failure:function(data){
-				debugger;
+		service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(params, function(places, status){
+			if(status === google.maps.places.PlacesServiceStatus.OK){
+				var current_infowindow;
+
+				_.each(places, function(place){
+					var marker =new google.maps.Marker({
+						position:{
+							'lat' : place.geometry.location.lat(),
+							'lng' : place.geometry.location.lng()
+						},
+						map : map
+					});
+
+					var infowindow_content = '<div id = content>' + 
+												'<h1 id="firstHeading">' + place.name + '</h1>' +
+											 '</div>';
+
+					var infowindow = new google.maps.InfoWindow({
+						content: infowindow_content
+					});
+
+					marker.addListener('click', function(){
+						if(current_infowindow){
+							current_infowindow.close();
+						}
+						infowindow.open(map, marker);
+						current_infowindow = infowindow;
+					});
+				});
+			}else{
+				window.alert(status);
 			}
 		});
 	}
