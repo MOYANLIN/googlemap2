576aaed6f4476a04826da6bb3cee1fa13a59971f
MichaeliaNa <nina.naliu1110@gmail.com>
2017-07-07 19:21:01 +0000

    center the triangle and hide the detailed info view if not active

--- js/mymap.js
+++ js/mymap.js
@@ -58,7 +58,6 @@ $(function(){
 				window.alert(status);
 			}
 		});
-
 		$('.place-info-visibility-toggle').on('click', function(){
 			$('#place-info-wrapper').toggleClass('visible');
 			$('#place-info-wrapper .triangle-icon').toggleClass('left');
@@ -75,6 +74,7 @@ $(function(){
 			$('.place-review-score').text(place['rating']);
 			$('.place-type').text(place['types'][0]);
 			$('#place-info-wrapper').addClass('visible');
+			$('#place-info-wrapper').addClass('is-active');
 		});
 	}
 	initMap();
